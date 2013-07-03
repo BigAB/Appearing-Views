c069fc37eae36f392a330f03b9b2b79b6aeecdb2
BigAB <abarrett@gasbuddy.com>
2013-07-03 11:48:54 -0600

    Adding Kiwi (and Cocoapods) to the project for testing

---
+++ Appearing Views Tests/ABAppearing View Tests.m
@@ -0,0 +1,21 @@
+//
+//  ABAppearing View Tests.m
+//  Appearing Views
+//
+//  Created by Adam Barrett on 2013-07-03.
+//  Copyright (c) 2013 Adam Barrett. All rights reserved.
+//
+#import "Kiwi.h"
+#import "ABAppearingView.h"
+
+SPEC_BEGIN(ABAppearingViewSpec)
+
+describe(@"Appearing", ^{
+    
+    it(@"should do something", ^{
+        [[ABAppearingView should] beNonNil];
+    });
+    
+});
+
+SPEC_END
---
+++ Podfile
@@ -0,0 +1,9 @@
+Podfile
+
+# Select the appropriate platform below
+platform :ios, '5.0'
+
+# Add Kiwi as an exclusive dependency for the target
+target :'Appearing Views Tests', :exclusive => true do
+   pod 'Kiwi'
+end
