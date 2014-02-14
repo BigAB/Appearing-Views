3808c16f182f1d0ae17921e3fa480fc7daf0afa7
BigAB <abarrett@gasbuddy.com>
2014-02-14 13:56:11 -0600

    Stopping multiple calls to appear or disappear from messing things up

--- Appearing Views/ABAppearingView.m
+++ Appearing Views/ABAppearingView.m
@@ -10,7 +10,7 @@
 
 static id<AnimationMachine> _animations;
 
-@interface ABAppearingView()
+@interface ABAppearingView ()
 
 @property (nonatomic, assign) CGRect fullFrame;
 @property (nonatomic, readonly) AVAnimationBlock appearancePrepBlock;
@@ -20,6 +20,9 @@ static id<AnimationMachine> _animations;
 
 @property (nonatomic, assign) BOOL originalClipsToBounds;
 
+@property (nonatomic, assign, getter = isAppearing) BOOL appearing;
+@property (nonatomic, assign, getter = isDisappearing) BOOL disappearing;
+
 - (void)viewWillAppearWithAnimation:(AnimationType)animation;
 - (void)viewDidAppearWithAnimation:(AnimationType)animation;
 - (void)viewWillDisappearWithAnimation:(AnimationType)animation;
@@ -27,26 +30,31 @@ static id<AnimationMachine> _animations;
 
 @end
 
-@implementation ABAppearingView{}
+@implementation ABAppearingView {}
 
 #pragma mark - Class Methods
-+(void)initialize
++ (void)initialize
 {
     _animations = [ABAnimations new];
 }
+
 
 + (void)setAnimationsObject:(id)animationsObject
 {
     _animations = animationsObject;
 }
 
-+ (UIWindow *)mainWindow {
+
++ (UIWindow *)mainWindow
+{
     NSLog(@"Windows: %@", [UIApplication sharedApplication].windows);
     UIWindow *window = [UIApplication sharedApplication].keyWindow;
-    if (!window || ![window isMemberOfClass:[UIWindow class]])
-        window = [[UIApplication sharedApplication].windows objectAtIndex:0];
+    
+    if (!window || ![window isMemberOfClass:[UIWindow class]]) window = [[UIApplication sharedApplication].windows objectAtIndex:0];
+    
     return window;
 }
+
 
 #pragma mark - LifeCycle
 - (void)_initialization
@@ -55,23 +63,30 @@ static id<AnimationMachine> _animations;
     self.animationDuration = 1.0f;
 }
 
+
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
+    
     if (self) {
         [self _initialization];
     }
+    
     return self;
 }
+
 
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
     self = [super initWithCoder:aDecoder];
+    
     if (self) {
         [self _initialization];
     }
+    
     return self;
 }
+
 
 #pragma mark - Property Accessors
 - (AVAnimationBlock)appearancePrepBlock
@@ -79,17 +94,20 @@ static id<AnimationMachine> _animations;
     return [_animations animationBlockWithType:self.animationType phase:AnimationPhasePrep];
 }
 
--(AVAnimationBlock)appearanceBlock
+
+- (AVAnimationBlock)appearanceBlock
 {
     return [_animations animationBlockWithType:self.animationType phase:AnimationPhaseIn];
 }
 
--(AVAnimationBlock)disappearanceBlock
+
+- (AVAnimationBlock)disappearanceBlock
 {
     return [_animations animationBlockWithType:self.animationType phase:AnimationPhaseOut];
 }
 
--(AVAnimationBlock)resetBlock
+
+- (AVAnimationBlock)resetBlock
 {
     return [_animations animationBlockWithType:self.animationType phase:AnimationPhaseReset];
 }
@@ -100,13 +118,16 @@ static id<AnimationMachine> _animations;
     if (!_notificationCenter) {
         _notificationCenter = [NSNotificationCenter defaultCenter];
     }
+    
     return _notificationCenter;
 }
+
 
 #pragma mark - Appearing and Disappearing
 - (void)appear
 {
-    if (!self.hidden) return;
+    if (!self.hidden || self.isAppearing) return;
+    self.appearing = YES;
     [self prepareForAnimatedAppearance];
     [self willCallbacksAndNotifications:AnimationPhaseIn];
     
@@ -114,77 +135,90 @@ static id<AnimationMachine> _animations;
     [self appearWithAnimation];
 }
 
+
 - (void)disappear
 {
-    if (self.hidden) return;
+    if (self.hidden || self.isDisappearing) return;
+    self.disappearing = YES;
     self.fullFrame = self.frame;
     [self willCallbacksAndNotifications:AnimationPhaseOut];
     [self disappearWithAnimation];
 }
+
 
 - (void)prepareForAnimatedAppearance
 {
     self.fullFrame = self.frame;
     self.originalClipsToBounds = self.clipsToBounds;
     self.clipsToBounds = YES;
-    self.appearancePrepBlock(self, self.fullFrame, self.animationDuration, ^(){});
+    self.appearancePrepBlock(self, self.fullFrame, self.animationDuration, ^() {});
 }
+
 
 - (void)appearWithAnimation
 {
     self.appearanceBlock(self, self.fullFrame, self.animationDuration, [self appearCompletionBlock]);
 }
 
+
 - (void)disappearWithAnimation
 {
     self.disappearanceBlock(self, self.fullFrame, self.animationDuration, [self disappearCompletionBlock]);
 }
 
+
 - (CompletionCallback)appearCompletionBlock
 {
     __weak typeof(self) _self = self;
-    return [^(){
+    return [^() {
         [_self didCallbacksAndNotifications:AnimationPhaseIn];
     } copy];
 }
 
+
 - (CompletionCallback)disappearCompletionBlock
 {
     __weak typeof(self) _self = self;
-    return [^(){
+    return [^() {
         _self.hidden = YES;
         [_self resetView];
         [_self didCallbacksAndNotifications:AnimationPhaseOut];
     } copy];
 }
 
+
 - (void)resetView
 {
     self.clipsToBounds = self.originalClipsToBounds;
     self.frame = self.fullFrame;
-    self.resetBlock(self, self.fullFrame, self.animationDuration, ^(){});
+    self.resetBlock(self, self.fullFrame, self.animationDuration, ^() {});
     self.fullFrame = CGRectZero;
 }
+
 
 - (void)viewWillAppearWithAnimation:(AnimationType)animation
 {
     // for subclassing
 }
 
+
 - (void)viewDidAppearWithAnimation:(AnimationType)animation
 {
     // for subclassing
 }
+
 
 - (void)viewWillDisappearWithAnimation:(AnimationType)animation
 {
     // for subclassing
 }
 
+
 - (void)viewDidDisappearWithAnimation:(AnimationType)animation
 {
     // for subclassing
 }
+
 
 #pragma mark - ABAppearingViewDelagate / Callbacks stuff
 - (void)willCallbacksAndNotifications:(AnimationPhase)phase
@@ -193,56 +227,69 @@ static id<AnimationMachine> _animations;
         [self notifyListenersWill:AnimationPhaseIn];
         [self viewWillAppearWithAnimation:self.animationType];
     }
+    
     if (phase == AnimationPhaseOut) {
         [self notifyListenersWill:AnimationPhaseOut];
         [self viewWillDisappearWithAnimation:self.animationType];
     }
 }
 
+
 - (void)didCallbacksAndNotifications:(AnimationPhase)phase
 {
     if (phase == AnimationPhaseIn) {
+        self.appearing = NO;
         [self notifyListenersDid:AnimationPhaseIn];
         [self viewDidAppearWithAnimation:self.animationType];
     }
+    
     if (phase == AnimationPhaseOut) {
+        self.disappearing = NO;
         [self notifyListenersDid:AnimationPhaseOut];
         [self viewDidDisappearWithAnimation:self.animationType];
     }
 }
 
+
 - (void)notifyListenersDid:(AnimationPhase)phase
 {
     NSDictionary *animationInfo = [self animationInfo];
+    
     switch (phase) {
         case AnimationPhaseIn:
+            
             if (self.delegate && [self.delegate respondsToSelector:@selector(viewDidAppear:withAnimation:)]) {
                 [self.delegate viewDidAppear:self withAnimation:self.animationType];
             }
+            
             [self.notificationCenter postNotificationName:ViewDidAppearWithAnimation object:self userInfo:animationInfo];
             
             if (self.viewDidAppearWithAnimationCallback) {
                 self.viewDidAppearWithAnimationCallback(self, animationInfo);
             }
+            
             break;
             
         case AnimationPhaseOut:
+            
             if (self.delegate && [self.delegate respondsToSelector:@selector(viewDidDisappear:withAnimation:)]) {
                 [self.delegate viewDidDisappear:self withAnimation:self.animationType];
             }
+            
             [self.notificationCenter postNotificationName:ViewDidDisappearWithAnimation object:self userInfo:animationInfo];
             
             if (self.viewDidDisappearWithAnimationCallback) {
                 self.viewDidDisappearWithAnimationCallback(self, animationInfo);
             }
+            
             break;
             
         case AnimationPhasePrep:
         default:
             break;
     }
-    
 }
+
 
 - (void)notifyListenersWill:(AnimationPhase)phase
 {
@@ -250,14 +297,17 @@ static id<AnimationMachine> _animations;
     
     switch (phase) {
         case AnimationPhaseIn:
+            
             if (self.delegate && [self.delegate respondsToSelector:@selector(viewWillAppear:withAnimation:toFrame:duration:options:)]) {
-                [self.delegate viewWillAppear:self
-                                withAnimation:[animationInfo[AppearingViewTypeKey] intValue]
-                                      toFrame:[animationInfo[AppearingViewFrameKey] CGRectValue]
-                                     duration:[animationInfo[AppearingViewDurationKey] floatValue]
-                                      options:[animationInfo[AppearingViewOptionsKey] intValue]
+                [self.delegate
+                 viewWillAppear:self
+                 withAnimation:[animationInfo[AppearingViewTypeKey] intValue]
+                 toFrame:[animationInfo[AppearingViewFrameKey] CGRectValue]
+                 duration:[animationInfo[AppearingViewDurationKey] floatValue]
+                 options:[animationInfo[AppearingViewOptionsKey] intValue]
                  ];
             }
+            
             [self.notificationCenter postNotificationName:ViewWillAppearWithAnimation object:self userInfo:animationInfo];
             
             if (self.viewWillAppearWithAnimationCallback) {
@@ -267,19 +317,23 @@ static id<AnimationMachine> _animations;
             break;
             
         case AnimationPhaseOut:
+            
             if (self.delegate && [self.delegate respondsToSelector:@selector(viewWillDisappear:withAnimation:fromFrame:duration:options:)]) {
-                [self.delegate viewWillDisappear:self
-                                   withAnimation:[animationInfo[AppearingViewTypeKey] intValue]
-                                       fromFrame:[animationInfo[AppearingViewFrameKey] CGRectValue]
-                                        duration:[animationInfo[AppearingViewDurationKey] floatValue]
-                                         options:[animationInfo[AppearingViewOptionsKey] intValue]
+                [self.delegate
+                 viewWillDisappear:self
+                 withAnimation:[animationInfo[AppearingViewTypeKey] intValue]
+                 fromFrame:[animationInfo[AppearingViewFrameKey] CGRectValue]
+                 duration:[animationInfo[AppearingViewDurationKey] floatValue]
+                 options:[animationInfo[AppearingViewOptionsKey] intValue]
                  ];
             }
+            
             [self.notificationCenter postNotificationName:ViewWillDisappearWithAnimation object:self userInfo:animationInfo];
             
             if (self.viewWillDisappearWithAnimationCallback) {
                 self.viewWillDisappearWithAnimationCallback(self, animationInfo);
             }
+            
             break;
             
         case AnimationPhasePrep:
@@ -288,15 +342,17 @@ static id<AnimationMachine> _animations;
     }
 }
 
+
 - (NSDictionary *)animationInfo
 {
     return @{
-             AppearingViewViewKey     : self,
-             AppearingViewFrameKey    : [NSValue valueWithCGRect:self.fullFrame],
-             AppearingViewTypeKey     : @(self.animationType),
-             AppearingViewDurationKey : @(self.animationDuration),
-             AppearingViewOptionsKey  : @(self.animationOptions),
+             AppearingViewViewKey: self,
+             AppearingViewFrameKey: [NSValue valueWithCGRect:self.fullFrame],
+             AppearingViewTypeKey: @(self.animationType),
+             AppearingViewDurationKey: @(self.animationDuration),
+             AppearingViewOptionsKey: @(self.animationOptions),
              };
 }
+
 
 @end
